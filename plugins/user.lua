40a80f8d4f1de51d770455e104b92d81255639c4
James Cartledge <james.cartledge@thoughtworks.com>
2023-07-26 16:34:50 +1000

    pilugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,5 @@
 return {
+  { "djoshea/vim-autoread", event = "User AstroFile" },
   { "gcmt/wildfire.vim", event = "User AstroFile" },
   { "jeffkreeftmeijer/vim-numbertoggle", event = "User AstroFile" },
 }
