b86d9618caf8f5c29ef7e8e4e9ffcbf600475169
James Cartledge <james.cartledge@thoughtworks.com>
2023-08-09 15:35:18 +1000

    better whitespace plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,6 +1,7 @@
 return {
   { "djoshea/vim-autoread", event = "User AstroFile" },
+  { "folke/trouble.nvim", opts = { auto_open = true, auto_close = true } },
   { "gcmt/wildfire.vim", event = "User AstroFile" },
   { "jeffkreeftmeijer/vim-numbertoggle", event = "User AstroFile" },
-  { "folke/trouble.nvim", opts = { auto_open = true, auto_close = true } },
+  { "ntpeters/vim-better-whitespace", event = "User AstroFile" },
 }
