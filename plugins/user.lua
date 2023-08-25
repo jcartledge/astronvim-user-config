14ef8687d81ad3d55ef0c1ebebb9075c20c28413
James Cartledge <james.cartledge@thoughtworks.com>
2023-08-25 13:09:03 +1000

    waikiki

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,5 +1,6 @@
 return {
   { "djoshea/vim-autoread", event = "User AstroFile" },
+  { "fcpg/vim-waikiki", lazy = false },
   { "folke/trouble.nvim", opts = { auto_open = true, auto_close = true } },
   { "gcmt/wildfire.vim", event = "User AstroFile" },
   { "jeffkreeftmeijer/vim-numbertoggle", event = "User AstroFile" },
