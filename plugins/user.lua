95bb98704a25a0c039c1120fff964695976a9774
James Cartledge <jcartledge@gmail.com>
2023-09-01 20:55:44 +1000

    auto_mkdir

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,9 +1,10 @@
 return {
-  { "dkarter/bullets.vim", event = "User AstroFile" },
   { "djoshea/vim-autoread", event = "User AstroFile" },
+  { "dkarter/bullets.vim", event = "User AstroFile" },
   { "fcpg/vim-waikiki", lazy = false },
   { "folke/trouble.nvim", opts = { auto_open = true, auto_close = true } },
   { "gcmt/wildfire.vim", event = "User AstroFile" },
   { "jeffkreeftmeijer/vim-numbertoggle", event = "User AstroFile" },
   { "ntpeters/vim-better-whitespace", event = "User AstroFile" },
+  { "vim-scripts/auto_mkdir", event = "User AstroFile" },
 }
